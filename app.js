7015c9ee586aa9d9d5163be793e82584ccdf7ae7
Yevhen R <7374618+yevhen-romanenko@users.noreply.github.com>
2020-09-07 00:28:31 +0300

    add config settings
    Add server and mongoDB connection settings

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('App!');
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = config.get("port") || 4000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    app.listen(5000, () => console.log(`app has been started on port ${PORT}...`));
+  } catch (e) {
+    console.log("Server error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
+
+
