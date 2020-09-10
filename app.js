ee577b90f4ddcf86e7fdba8e8cb65db8b6825dd7
Yevhen R <7374618+yevhen-romanenko@users.noreply.github.com>
2020-09-10 04:38:54 +0300

    config and routes implement
    Implement configuration of routes 'login' and 'registration'

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const config = require("config");
-const mongoose = require("mongoose");
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = config.get("port") || 4000;
+app.use('/api/auth', require('./routes/auth.routes'));
+
+const PORT = config.get('port') || 5000;
 
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     app.listen(5000, () => console.log(`app has been started on port ${PORT}...`));
   } catch (e) {
-    console.log("Server error", e.message);
+    console.log('Server error', e.message);
     process.exit(1);
   }
 }
 
 start();
-
-
